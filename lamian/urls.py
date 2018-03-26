69ed0bc41a0df7649222cf23dde4c4b8e406c221
yinda0126@gmail.com <yinda0126@gmail.com>
2018-03-25 23:19:24 -0700

    html update

--- lamian/urls.py
+++ lamian/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    url(r'^index$', views.index),
     url(r'^menu$', views.menu),
-    url(r'^index$', views.index),
-    url(r'^index$', views.index),
+    url(r'^contact$', views.contact),
+    url(r'^about$', views.about),
+    url(r'^localisation$', views.localisation),
+
 ]
--- lamian/views.py
+++ lamian/views.py
@@ -4,11 +4,29 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    context['index'] = ['active']
+    context['flag_index'] = ['active']
     return render(request, 'lamian/index.html', context)
 
 
 def menu(request):
     context = {}
-    context['menu'] = ['active']
+    context['flag_menu'] = ['active']
     return render(request, 'lamian/menu.html', context)
+
+
+def contact(request):
+    context = {}
+    context['flag_contact'] = ['active']
+    return render(request, 'lamian/contact.html', context)
+
+
+def localisation(request):
+    context = {}
+    context['flag_localisation'] = ['active']
+    return render(request, 'lamian/localisation.html', context)
+
+
+def about(request):
+    context = {}
+    context['flag_about'] = ['active']
+    return render(request, 'lamian/about.html', context)
